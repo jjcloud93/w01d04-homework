bd0c201307848a4588c17745974fddac652a4252
James M <saintjames@MacBook-Pro-2.local>
2018-06-14 17:00:06 -0600

    Commit 2 - Palindrome

--- js/solutions.js
+++ js/solutions.js
@@ -8,3 +8,105 @@
 
 //Palindrome
 
+const checkPalindrome = (string) => {
+	let data = string.length;
+for(i = 0; i < Math.floor(data/2); i++) {
+	if (string[i] !== string[data - 1 - i]) {
+		return false;
+	}
+}
+	return true;
+}
+console.log(checkPalindrome('radar'));
+
+
+
+const checkPalindrome2 = (string) => {
+  let lowerCase = string.toLowerCase();
+  let reverseString = lowerCase.split('').reverse('').join('');
+  if (lowerCase === reverseString) {
+    return true;
+  }else {
+    return false;
+  }
+}
+console.log(checkPalindrome2("Radar"));
+
+//Digit Sum
+
+const sumDigits. = () => {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
