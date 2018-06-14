c5aab5c32eb70cf4eaa63eac8372841fe5997c64
James M <saintjames@MacBook-Pro-2.local>
2018-06-14 16:16:10 -0600

    Commit 1 - Verbal questions

---
+++ js/solutions.js
@@ -0,0 +1,10 @@
+//Verbal questions
+
+// Parameters are variables inside a function, while an argument
+// is the value of the variable(parameter).
+
+// Console.log is only useful to the programmer for issues like
+// debugging, while return pushes the value to the next code.
+
+//Palindrome
+
